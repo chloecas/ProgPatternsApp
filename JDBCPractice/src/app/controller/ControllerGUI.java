fa628fc188001598d1ef4a95a47391b5db4447a8
Emelie <ememejia2106@gmail.com>
2026-04-14 18:19:52 -0400

    visual added (test only)

--- JDBCPractice/src/app/controller/ControllerGUI.java
+++ JDBCPractice/src/app/controller/ControllerGUI.java
@@ -3,9 +3,17 @@ package app.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+import javafx.scene.layout.Pane;
+
 public class ControllerGUI {
+
+    @FXML
+    private Pane loginPage;
+
     @FXML
     private void handleClick(ActionEvent event) {
         System.out.println("Button clicked!");
+        loginPage.setVisible(true);
+
     }
 }
